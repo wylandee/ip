e0d1c80c2b35a75320b9ca19917d6bd8f990e302
dylan <mixedberries92@gmail.com>
2025-08-19 17:37:38 +0800

    Update Main logic to use newly created Message classes

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -1,10 +1,6 @@
 public class David {
     public static void main(String[] args) {
-        String horizontalLine = "----------------------------------";
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm David\nWhat can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        System.out.println(new Greeting());
+        System.out.println(new Goodbye());
     }
 }
