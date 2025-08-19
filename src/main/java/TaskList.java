a7f940a2b4b086fa988501a4940277c3a2d0dc2e
dylan <mixedberries92@gmail.com>
2025-08-20 02:14:36 +0800

    Add TaskList logic

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,30 @@
+import java.util.Arrays;
+
 public class TaskList {
+    Task[] items;
+
+    public TaskList() {
+        items = new Task[100];
+    }
+
+    public void addTask(Task task) {
+        for (int i = 0; i < 100; i++) {
+            if (items[i] == null) {
+                items[i] = task;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            if (items[i] == null) {
+                break;
+            }
+            sb.append(i + 1).append(". ").append(items[i]).append("\n");
+        }
+        return sb.toString();
+    }
 }
