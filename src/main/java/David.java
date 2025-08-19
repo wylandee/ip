51baa7ededf5c831a1a1d2e1932f1aad7d6db99a
dylan <mixedberries92@gmail.com>
2025-08-19 16:33:00 +0800

    Rename main class, add greeting, update .gitignore to ignore .class files

---
+++ src/main/java/David.java
@@ -0,0 +1,10 @@
+public class David {
+    public static void main(String[] args) {
+        String horizontalLine = "----------------------------------";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm David\nWhat can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
