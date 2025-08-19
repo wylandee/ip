24040582bc1c49d44cc5512d08849a207afe681f
dylan <mixedberries92@gmail.com>
2025-08-19 17:44:17 +0800

    Add echo and end conversation logic

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class David {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(new Greeting());
+        String word = sc.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(new Message(word));
+            word = sc.nextLine();
+        }
+
         System.out.println(new Goodbye());
     }
 }
