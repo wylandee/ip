beafe59829e22f658c47c9880da5cc1a439632fc
dylan <mixedberries92@gmail.com>
2025-08-20 03:24:40 +0800

    Update logic to add task and show task list

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class David {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        TaskList tl = new TaskList();
 
         System.out.println(new Greeting());
         String word = sc.nextLine();
         while (!word.equals("bye")) {
-            System.out.println(new Message(word));
+            if (word.equals("list")) {
+                System.out.println(new ListMessage(tl));
+            } else {
+                System.out.println(new AddMessage(word));
+                tl.addTask(new Task(word));
+            }
             word = sc.nextLine();
         }
 
