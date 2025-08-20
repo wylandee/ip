c8db26e43536552ccd2db53ed92a07005832ab48
dylan <mixedberries92@gmail.com>
2025-08-20 14:32:51 +0800

    Add mark task logic to Main class

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -6,15 +6,20 @@ public class David {
         TaskList tl = new TaskList();
 
         System.out.println(new Greeting());
-        String word = sc.nextLine();
+        String word = sc.next();
         while (!word.equals("bye")) {
             if (word.equals("list")) {
                 System.out.println(new ListMessage(tl));
+            } else if (word.equals("mark")) {
+                int i = sc.nextInt();
+                Task curr = tl.getTask(i);
+                curr.markAsDone();
+                System.out.println(new MarkMessage(curr.toString()));
             } else {
                 System.out.println(new AddMessage(word));
                 tl.addTask(new Task(word));
             }
-            word = sc.nextLine();
+        word = sc.next();
         }
 
         System.out.println(new Goodbye());
