f09aed0d12f3b3790753c79b11ac33396d2d9ade
dylan <mixedberries92@gmail.com>
2025-08-20 14:31:40 +0800

    Add string outpur for marking task

--- src/main/java/MarkMessage.java
+++ src/main/java/MarkMessage.java
@@ -1,2 +1,11 @@
-public class MarkMessage {
+public class MarkMessage extends Message {
+
+    public MarkMessage(String text) {
+        super("I have marked this task as done\n" + text);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
