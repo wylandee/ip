67a77c7f78f5c848d19c435583791e193361b039
dylan <mixedberries92@gmail.com>
2025-08-20 10:15:01 +0800

    Add logic for Task completion

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,17 @@
 public class Task {
     String text;
+    boolean isDone;
     public Task(String text) {
         this.text = text;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 
     @Override
     public String toString() {
-        return text;
+        return String.format("[%s] %s", this.getStatusIcon(), text);
     }
 }
