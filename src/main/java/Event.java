767e69f42ff54482e2e6be1c5ba6fd512f181d6d
dylan <mixedberries92@gmail.com>
2025-08-20 15:29:34 +0800

    Add Event logic

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,15 @@
-public class Event {
+public class Event extends Task {
+    String from;
+    String to;
+
+    public Event(String text, String from, String to) {
+        super(text);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
 }
