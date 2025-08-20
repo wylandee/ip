1568a1b572879bd8a8c8135a1cbf1afa4d767751
dylan <mixedberries92@gmail.com>
2025-08-20 15:40:57 +0800

    Change if-else conditionals to switch statements

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -8,26 +8,41 @@ public class David {
         System.out.println(new Greeting());
         String word = sc.next();
         while (!word.equals("bye")) {
-            if (word.equals("list")) {
-                System.out.println(new ListMessage(tl));
-            } else if (word.equals("mark")) {
-                int i = sc.nextInt();
-                Task curr = tl.getTask(i);
-                curr.markAsDone();
-                System.out.println(new MarkMessage(curr.toString()));
-            } else if (word.equals("deadline")) {
-                String[] line = sc.nextLine().split("/by");
-                tl.addTask(new Deadline(line[0].stripTrailing(), line[1].stripLeading()));
-                System.out.println(new AddMessage(line[0].stripTrailing()));
-            } else if (word.equals("event")) {
-                String[] line = sc.nextLine().split("/from");
-                String[] times = line[1].split("/to");
-                tl.addTask(new Event(line[0].stripTrailing(), times[0].stripLeading().stripTrailing(), times[1].stripLeading()));
-                System.out.println(new AddMessage(line[0].stripTrailing()));
-            } else if (word.equals("todo")){
-                String line = sc.nextLine();
-                tl.addTask(new Todo(line));
-                System.out.println(new AddMessage(line));
+            switch (word) {
+                case "list":
+                    System.out.println(new ListMessage(tl));
+                    break;
+
+                case "mark":
+                    int i = sc.nextInt();
+                    Task curr = tl.getTask(i);
+                    curr.markAsDone();
+                    System.out.println(new MarkMessage(curr.toString()));
+                    break;
+
+                case "deadline":
+                    String[] line = sc.nextLine().split("/by");
+                    tl.addTask(new Deadline(line[0].stripTrailing(), line[1].stripLeading()));
+                    System.out.println(new AddMessage(line[0].stripTrailing()));
+                    break;
+
+                case "event":
+                    String[] eventLine = sc.nextLine().split("/from");
+                    String[] times = eventLine[1].split("/to");
+                    tl.addTask(new Event(
+                            eventLine[0].stripTrailing(),
+                            times[0].stripLeading().stripTrailing(),
+                            times[1].stripLeading()
+                    ));
+                    System.out.println(new AddMessage(eventLine[0].stripTrailing()));
+                    break;
+
+                case "todo":
+                    String lineTodo = sc.nextLine();
+                    tl.addTask(new Todo(lineTodo));
+                    System.out.println(new AddMessage(lineTodo));
+                    break;
+
             }
         word = sc.next();
         }
