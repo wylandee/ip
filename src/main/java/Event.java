640620f23a0d13c2ed17e64207772527fb2a3148
dylan <mixedberries92@gmail.com>
2025-08-26 20:46:36 +0800

    Update date and time strings to DateTime objects and proper formatting for deadline and event tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,23 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String from;
-    private String to;
+    private LocalDateTime from;
+    private LocalDateTime to;
 
     public Event(String text, String from, String to) {
         super(text);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.from = LocalDateTime.parse(from, inputFormat);
+        this.to = LocalDateTime.parse(to, inputFormat);
     }
 
-    public String getFrom() {
+    public LocalDateTime getFrom() {
         return this.from;
     }
 
-    public String getTo() {
+    public LocalDateTime getTo() {
         return this.to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM d yyyy, h:mma");
+        return "[E]" + super.toString() + " (from: " + this.from.format(outputFormat) + " to: " + this.to.format(outputFormat) + ")";
     }
 }
