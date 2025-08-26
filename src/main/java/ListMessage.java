b1d29965d186cef219862c725b0e6cf40c8a133c
dylan <mixedberries92@gmail.com>
2025-08-27 00:51:27 +0800

    Remove Message class and child classes

--- src/main/java/ListMessage.java
+++
@@ -1,9 +0,0 @@
-public class ListMessage extends Message {
-    public ListMessage(TaskList tl) {
-        super("Wah these are the tasks in your list:\n" + tl.toString());
-    }
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
