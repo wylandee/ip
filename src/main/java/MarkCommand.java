c48ef50ec6940d2f074aeecc7903c3ca4047cbce
dylan <mixedberries92@gmail.com>
2025-08-27 00:43:59 +0800

    Create MarkCommand and DeleteCommand child classes, move mark and delete logic from main

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,19 @@
+public class MarkCommand extends Command {
+    private int index;
+
+    public MarkCommand(String args) throws DukeException {
+        try {
+            this.index = Integer.parseInt(args.trim());
+        } catch (NumberFormatException e) {
+            throw new DukeException("Eh give an index number after mark leh");
+        }
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        Task task = tasks.getTask(index);
+        tasks.markAsDone(index);
+        ui.showMessage("    Ok I mark as done liao:\n    " + task);
+        storage.save(tasks);
+    }
+}
