8baab51e4102a361b0fcfdb06fbefccc70b391b6
dylan <mixedberries92@gmail.com>
2025-08-27 02:55:17 +0800

    Create parser, storage, ui packages and move their classes into them

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -1,6 +1,9 @@
 import command.Command;
 import exception.DukeException;
+import parser.Parser;
+import storage.Storage;
 import task.TaskList;
+import ui.Ui;
 
 public class David {
     private Storage storage;
