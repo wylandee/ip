cc9651ad74db157dd9ac7b659233431b49d73f8e
dylan <mixedberries92@gmail.com>
2025-08-26 23:49:05 +0800

    Add command parsing logic, move switch statements from main class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,25 @@
 public class Parser {
+    public static Command parse(String fullCommand) throws DukeException {
+        String[] parts = fullCommand.split(" ", 2);
+        String commandWord = parts[0];
+
+        switch (commandWord) {
+            case "list":
+                return new ListCommand();
+            case "mark":
+                return new MarkCommand(parts[1]);
+            case "deadline":
+                return new DeadlineCommand(parts[1]);
+            case "event":
+                return new EventCommand(parts[1]);
+            case "todo":
+                return new TodoCommand(parts[1]);
+            case "delete":
+                return new DeleteCommand(parts[1]);
+            case "bye":
+                return new ExitCommand();
+            default:
+                throw new NoCommandException("Eh idk what you saying bro, use one of the commands");
+        }
+    }
 }
