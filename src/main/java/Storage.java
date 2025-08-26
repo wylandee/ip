5db9880c1b77a105e8e06e31dcb4f8fa0333a6ef
dylan <mixedberries92@gmail.com>
2025-08-26 14:32:51 +0800

    Add method to check data file and folder exists

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,26 @@
+import java.io.*;
+import java.nio.file.*;
+
 public class Storage {
+    private final Path filePath;
+
+    public Storage(String filePath) {
+        this.filePath = Paths.get(filePath);
+        checkFileAndFolderExists();
+    }
+
+    private void checkFileAndFolderExists() {
+        try {
+            if (!Files.exists(filePath.getParent())) {
+                Files.createDirectories(filePath.getParent());
+            }
+            if (!Files.exists(filePath)) {
+                Files.createFile(filePath);
+            }
+        } catch (IOException e) {
+            System.err.println("Eh you got an error creating data file: " + e.getMessage());
+        }
+    }
+
+    
 }
