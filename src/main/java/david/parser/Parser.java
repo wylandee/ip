801481eab088d6afae9b10022d7e7e37b2f835ea
dylan <mixedberries92@gmail.com>
2025-08-27 23:09:41 +0800

    Update exception logic to catch inputs less than the needed number of words

--- src/main/java/david/parser/Parser.java
+++ src/main/java/david/parser/Parser.java
@@ -2,26 +2,36 @@ package david.parser;
 
 import david.command.*;
 import david.exception.DukeException;
+import david.exception.FindException;
 import david.exception.NoCommandException;
 
 public class Parser {
     public static Command parse(String fullCommand) throws DukeException {
         String[] parts = fullCommand.split(" ", 2);
         String commandWord = parts[0];
+        String args;
 
         switch (commandWord) {
             case "list":
                 return new ListCommand();
             case "mark":
-                return new MarkCommand(parts[1]);
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new MarkCommand(args);
             case "deadline":
-                return new DeadlineCommand(parts[1]);
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new DeadlineCommand(args);
             case "event":
-                return new EventCommand(parts[1]);
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new EventCommand(args);
             case "todo":
-                return new TodoCommand(parts[1]);
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new TodoCommand(args);
             case "delete":
-                return new DeleteCommand(parts[1]);
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new DeleteCommand(args);
+            case "find":
+                args = (parts.length < 2 ? "" : parts[1]);
+                return new FindCommand(args);
             case "bye":
                 return new ExitCommand();
             default:
