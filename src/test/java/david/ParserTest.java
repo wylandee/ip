74eba822076765979439e57a77aab893a580dd21
dylan <mixedberries92@gmail.com>
2025-08-27 16:46:25 +0800

    Create test folder, create ParserTest class and test 3 methods

---
+++ src/test/java/david/ParserTest.java
@@ -0,0 +1,27 @@
+package david.parser;
+
+import david.command.*;
+import david.exception.DukeException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void parse_validTodoCommand_returnsTodoCommand() throws DukeException {
+        Command c = Parser.parse("todo read book");
+        assertTrue(c instanceof TodoCommand, "Expected a TodoCommand");
+    }
+
+    @Test
+    void parse_invalidCommand_throwsException() {
+        assertThrows(DukeException.class, () -> Parser.parse("blah blah blah"));
+    }
+
+    @Test
+    void parse_deadlineWithDate_returnsDeadlineCommand() throws DukeException {
+        Command c = Parser.parse("deadline return book /by 2019-12-02 2359");
+        assertTrue(c instanceof DeadlineCommand, "Expected a DeadlineCommand");
+    }
+}
