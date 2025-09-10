c61fe9bcf36bf8c92b351d6a16577d8205b935f6
dylan <mixedberries92@gmail.com>
2025-09-11 04:07:23 +0800

    Create CommandHistory class

---
+++ src/main/java/david/command/CommandHistory.java
@@ -0,0 +1,25 @@
+package david.command;
+
+import java.util.Stack;
+
+import david.exception.DavidException;
+import david.storage.Storage;
+import david.task.TaskList;
+
+public class CommandHistory {
+    private Stack<Command> history = new Stack<>();
+
+    public void add(Command c) {
+        if (c.isUndoable()) {
+            history.push(c);
+        }
+    }
+
+    public void undo(TaskList tasks, Storage storage) throws DavidException {
+        if (history.isEmpty()) {
+            throw new DavidException("Eh got nothing to undo lah");
+        }
+        Command top = history.pop();
+        top.undo(tasks, storage);
+    }
+}
